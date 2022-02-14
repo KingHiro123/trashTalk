61e7a7281520f410b8e00fa392202342e5920434
Dzarool <98579496+Dzarool@users.noreply.github.com>
2022-02-14 22:53:01 +0800

    Delete Cart.py

--- Cart.py
+++
@@ -1,8 +0,0 @@
-from Base import *
-from datetime import datetime
-
-class Cart(Base):
-    def __init__(self, user_id, cart_id):
-        Base.__init__(self, datetime.now())
-        self.total = 0
-        self.items = {}
