ca302cc946d9ae7e207d7f94e404d174c7cb0b85
joana <sjoana448@gmail.com>
2022-02-15 14:38:50 +0800

    Joana - AM I ABLE TO SEND THIS

--- Base.py
+++ Base.py
@@ -2,7 +2,7 @@ import datetime
 class Base:
     def __init__(self):
         self.__date = datetime.datetime.now()
-
+    
     def set_created_date(self, created_date):
         self.__date = created_date
 
--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, url_for, redirect, request, flash, session
 from flask_rbac import RBAC
+from classes.cart import Cart
 from Forms import Signup_Form, Login_Form, CreateVoucherForm
 import shelve, signUp, Voucher
 from flask_login import current_user, login_required, login_user, logout_user, LoginManager
@@ -18,7 +19,34 @@ def load_user(user_id):
     return signup_dict.get(user_id)
 
 app.config['SECRET_KEY'] = 'bananaisagoodfruit'
-@app.route("/")
+
+#forget password
+#account list design
+#update account list design 
+#dashboard(?)
+#integration (rachel, josh)
+#profile for customer
+#
+
+#Affan's code
+@app.route('/')
+def productpage():
+    # user_accounts = {}
+    # db = shelve.open('user.db', 'r')
+    # try:
+    # if 'User' in db:
+    # user_accounts = db['User']
+    # else:
+    # db['User'] = user_accounts
+    # except:
+    # print("Error in retrieving Items from user.db.")
+    # db.close()
+
+    # get username of account logged in
+    return render_template('productpg.html')  # , username=username)
+    
+#Joana's Code
+@app.route("/admin")
 def home():
     return render_template("Home_Admin.html", current_user=current_user)
 
@@ -45,7 +73,7 @@ def login():
                 #the admin is still being tested, once the group has their code integrated
                 if attempted_username == 'admin' and attempted_password == 'password':
                     login_user(user)
-                    return redirect(url_for('home'))
+                    return redirect(url_for('home'), current_user=attempted_username)
                 else:
                     if login.username.data != user.get_username() or login.password.data != user.get_password():
                         flash(f'Invalid username or password! Please check your login details and try again.', 'warning')
@@ -57,7 +85,7 @@ def login():
             db.close()
 
             print(user)
-            return redirect(url_for('home', current_user=current_user))   
+            return redirect(url_for('productpage', current_user=current_user))   
 
         except IOError:
             print("Error, it does not exist")
@@ -83,15 +111,15 @@ def signup():
                     user = signup_dict[key]
 
                     if sign_up.username.data == user.get_username():
-                        flash('This username has already been used.')
+                        flash('This username has already been used.', 'warning')
 
                         return redirect(url_for('signup'))
 
                     elif sign_up.email.data == user.get_email():
-                        flash('This email has already been used.')
+                        flash('This email has already been used.', 'warning')
 
                         return redirect(url_for('signup'))
-                    
+
             s_up = signUp.Signup(sign_up.username.data, sign_up.email.data, sign_up.password.data, sign_up.confirmpass.data)
 
             flash(f'Account created for {sign_up.username.data}!', 'success')
@@ -102,7 +130,7 @@ def signup():
             db['signUp'] = signup_dict
             db.close()
 
-            return redirect(url_for('acc_list'))
+            return redirect(url_for('home'))
         else:
             s_up = signUp.Signup(sign_up.username.data, sign_up.email.data, sign_up.password.data, sign_up.confirmpass.data)
 
@@ -111,7 +139,7 @@ def signup():
             db['signUp'] = signup_dict
             db.close()
 
-            return redirect(url_for('acc_list'))
+            return redirect(url_for('home'))
 
     return render_template('Signup_Tryouts.html', form=sign_up)
 
@@ -120,14 +148,14 @@ def signup():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('productpage'))
 
 #Authentication
 @app.route('/login_authentication', methods=['GET', 'POST'])
 def auth():
     if request.method == 'GET':
         if current_user.is_authenticated:
-            return render_template("Home_Admin.html")
+            return render_template("productpg.html")
         else:
             return render_template("Login.html")
 
@@ -231,6 +259,28 @@ def claimed_rewards():
 
     return render_template('claimedRewards.html', count=len(vouchers_list), vouchers_list=vouchers_list)
 
+@app.route('/claimVoucher/<int:id>', methods=['GET', 'POST'])
+@login_required
+def claim_voucher(id):
+
+        vouchers_dict = {}
+        db = shelve.open('voucher.db', 'c')
+
+        try:
+            vouchers_dict = db['Vouchers']
+        except:
+            print("Error in retrieving Vouchers from voucher.db.")
+        print(vouchers_dict)
+        print(id)
+        voucher = vouchers_dict[id]
+        voucher.set_claimed(current_user.get_id())
+        vouchers_dict[id] = voucher
+        db['Vouchers'] = vouchers_dict
+
+        db.close()
+
+        return redirect(url_for('retrieve_vouchers'))
+
 @app.route('/createVoucher', methods=['GET', 'POST'])
 def create_voucher():
     create_voucher_form = CreateVoucherForm(request.form)
@@ -315,5 +365,232 @@ def delete_voucher(id):
 
     return redirect(url_for('retrieve_vouchers'))
 
+
+
+#affan's code
+@app.route('/addtocart/', methods=['GET', 'POST'])
+def addtocart():
+    user_cart = {}
+    db = shelve.open('cart.db', 'c')
+
+    try:
+        if 'Cart' in db:
+            user_cart = db['Cart']
+        else:
+            db['Cart'] = user_cart
+    except:
+        print("Error in retrieving Items from cart.db.")
+
+    cart_id = 1
+    c = Cart(cart_id)
+    c.add_item(item_name=request.json['name'], quantity=1, price=request.json['price'])
+    print(c.get_total())
+
+    try:
+        cart = user_cart.get(cart_id)
+        cart.update(c.get_total())
+        user_cart[cart_id] = cart
+    except:
+        user_cart[cart_id] = (c.get_total())
+
+    print(user_cart.get(cart_id))
+    db['Cart'] = user_cart
+
+    db.close()
+    print('Add cart successful')
+    return 's'
+
+
+@app.route('/usercart')
+def usercart():
+    user_cart = {}
+    db = shelve.open('cart.db', 'r')
+    try:
+        if 'Cart' in db:
+            user_cart = db['Cart']
+        else:
+            db['Cart'] = user_cart
+    except:
+        print("Error in retrieving Items from cart.db.")
+    db.close()
+
+    cart_id = 1
+    product_list = user_cart.get(cart_id)
+
+    if product_list == None:
+        product_list = {"No products yet": [0, 0]}
+
+    print(product_list)
+
+    return render_template('usercart.html', product_list=product_list)
+
+
+@app.route('/additem/<itemname><int:quantity>', methods=['POST'])
+def additem(itemname, quantity):
+    user_cart = {}
+    db = shelve.open('cart.db', 'w')
+    user_cart = db['Cart']
+
+    cart_id = 1
+    quantity += 1
+    product_list = user_cart.get(cart_id)
+    product_list[itemname][1] = int(quantity)
+    print(product_list)
+    user_cart[cart_id] = product_list
+    db['Cart'] = user_cart
+
+    db.close()
+    return render_template('usercart.html', product_list=product_list)
+
+
+@app.route('/removeitem/<itemname><int:quantity>', methods=['POST'])
+def removeitem(itemname, quantity):
+    user_cart = {}
+    db = shelve.open('cart.db', 'w')
+    user_cart = db['Cart']
+
+    cart_id = 1
+    quantity -= 1
+    product_list = user_cart.get(cart_id)
+    if quantity == 0:
+        product_list.pop(itemname)
+    else:
+        product_list[itemname][1] = int(quantity)
+    print(product_list)
+    user_cart[cart_id] = product_list
+    db['Cart'] = user_cart
+
+    db.close()
+    return render_template('usercart.html', product_list=product_list)
+
+
+@app.route('/mice')
+def mice():
+    return render_template('mice.html')
+
+
+@app.route('/audio')
+def audio():
+    return render_template('audio.html')
+
+
+@app.route('/keyboards')
+def keyboards():
+    return render_template('keyboards.html')
+
+
+@app.route('/monitors')
+def monitors():
+    return render_template('monitors.html')
+
+
+@app.route('/rogmonitor')
+def rogmonitor():
+    return render_template('ROGmonitor.html')
+
+
+@app.route('/razerkraken')
+def razerkraken():
+    return render_template('razerkraken.html')
+
+
+@app.route('/logimouse')
+def logimouse():
+    return render_template('logimouse.html')
+
+
+@app.route('/vipermini')
+def vipermini():
+    return render_template('vipermini.html')
+
+
+@app.route('/corsairkeyboard')
+def corsairkeyboard():
+    return render_template('corsairkeyboard.html')
+
+
+@app.route('/infopage')
+def infopage():
+    user_cart = {}
+    db = shelve.open('cart.db', 'r')
+    try:
+        if 'Cart' in db:
+            user_cart = db['Cart']
+        else:
+            db['Cart'] = user_cart
+    except:
+        print("Error in retrieving Items from cart.db.")
+    db.close()
+
+    cart_id = 1
+    try:
+        product_list = user_cart.get(cart_id)
+    except:
+        product_list = {"No products yet": [0, 0]}
+
+    print(product_list)
+    return render_template('infopage.html', product_list=product_list)
+
+
+@app.route('/paymentpage')
+def paymentpage():
+    user_cart = {}
+    db = shelve.open('cart.db', 'r')
+    try:
+        if 'Cart' in db:
+            user_cart = db['Cart']
+        else:
+            db['Cart'] = user_cart
+    except:
+        print("Error in retrieving Items from cart.db.")
+    db.close()
+
+    cart_id = 1
+    try:
+        product_list = user_cart.get(cart_id)
+    except:
+        product_list = {"No products yet": [0, 0]}
+
+    print(product_list)
+
+    vouchers_dict = {}
+    db = shelve.open('voucher.db', 'r')
+    vouchers_dict = db['Vouchers']
+    db.close()
+
+    vouchers_list = []
+    for key in vouchers_dict:
+        voucher = vouchers_dict.get(key)
+        vouchers_list.append(voucher)
+
+    print(vouchers_list)
+    return render_template('paymentpage.html', product_list=product_list, vouchers_list=vouchers_list)
+
+
+@app.route('/complete')
+def completeorder():
+    user_cart = {}
+    db = shelve.open('cart.db', 'w')
+    user_cart = db['Cart']
+
+    cart_id = 1
+    product_list = user_cart.get(cart_id)
+    user_cart.pop(cart_id)
+    print(product_list)
+
+    # also minus product from itemdb storage
+    # specific_dict = {}
+    # db = shelve.open('stock.db', 'w')
+    # specific_dict = db['Details']
+
+    # print()
+    # for key in product_list:      # key is the itemname
+    #   specific_dict[key][quantity] -= productlist[key][2]     # make it to int() ?
+
+    # db['Details'] = specific_dict
+    # db.close()
+    db['Cart'] = user_cart
+    db.close()
+    return render_template('productpg.html')
 if __name__ == "__main__":
     app.run(debug=True)
